3ae1201ee6d01f04923bb4ab71319f9298955223
JAHIDIN SHOLEH <43899372+Jahidin123@users.noreply.github.com>
2018-10-09 22:51:01 +0700

    Add files via upload

---
+++ menentukan ganjil dan genap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+int main(int argc, char const *argv[])
+{ int nilai,hasil;
+	cout<<"menentukan ganjil dan genap"<<endl;
+	cout<<10;
+	cin>>nilai;
+	hasil=nilai%2;
+	cout<<"hasilnya adalah =";
+	if (hasil==0)
+		cout<<"genap";
+	else
+		cout<<"ganjil";
+
+	return 0;
+}
