73f664a991d66b6f85f37f9ee465d12684159601
JAHIDIN SHOLEH <43899372+Jahidin123@users.noreply.github.com>
2018-10-10 08:29:51 +0700

    Add files via upload

---
+++ menentukan nilai terbesar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int a,b;
+	cout<<"menentukan nilai terbesar";
+	cout<<"masukan nilai 1:";
+	cin>>a;
+	cout<<"masukan nila 2:";
+	cin>>b;
+
+	if (a>b)
+		cout<<"nilai terbesar:"<< a <<endl;
+	else
+		cout<<"nilai terbesar"<< b <<endl;
+		
+		return 0;
+
+}
